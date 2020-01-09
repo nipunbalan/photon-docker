5db9c07af51cd9b9431760f8e27fa61ae8d875a0
Nipun Balan Thekkummal <nipunbalan@gmail.com>
2020-01-09 01:03:13 +0000

    Pull photon binary directly from git

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ if [ ! -d "/photon/photon_data/elasticsearch" ]; then
     echo "Downloading search index"
 
     # Let graphhopper know where the traffic is coming from
-    USER_AGENT="docker: thomasnordquist/photon-geocoder"
-    wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x
+    USER_AGENT="docker: nipunbalan/photon-geocoder"
+    wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | pbzip2 -cd | tar x
 fi
 
 # Start photon if elastic index exists
